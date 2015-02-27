e3363bd5dc08051aa6bcb59ff2e288462913e3c4
ThornWinters <MichaelJWarner@hotmail.com>
2015-02-26 21:43:41 -0800

    connected the math

--- input.js
+++ input.js
@@ -1,13 +1,37 @@
 
+var currQuestion = "Loading...";
+var currAnswer = "Loading...";
+
 function input(){
 
 	
 	$(".input").append( "<div>Answer</div>");
-	$(".input").append("<input type='text' name='firstname'>");
+	$(".input").append("<input id='input' type='text' name='firstname'>");
 	$(".input").keypress(function(e) {
+		$("#input").removeClass('red');
 	    if(e.which == 13) {
-	        alert('You pressed enter!');
+	    	console.log($("#input").val())
+	    	if($("#input").val() === (currAnswer + "")){
+	        	questionCreate();
+	        	$("#input").val('');
+	        }else{
+	        	$("#input").addClass('red');
+	        }
 	    }
 	});
+	questionCreate();
+}
 
+function questionCreate(){
+	newQuestion();
+	drawQuestion();
+}
+
+function newQuestion (){
+	var question = generateQuestions(1);
+	currQuestion = question[0][0];
+	currAnswer = question[0][1];
+}
+function drawQuestion(){
+	$('.Question').html("<div>" + currQuestion + "</div>")
 }
